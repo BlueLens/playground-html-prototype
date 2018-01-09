12457f3c47f67c6ad5e6e22c5b8cee519793bb6c
Ahreum Han <reum712@gmail.com>
2018-01-09 19:30:15 +0900

    Image upload preview fixed
    다른 스크립트 수정중에 업로드 시 프리뷰가 안 되도록 바뀌었었네요 ㅠ

--- dist/stylens.js
+++ dist/stylens.js
@@ -1,15 +1,13 @@
-function readURL(input) {
-    if (input.files && input.files[0]) {
+function readURL(input){
+    if (input.files && input.files[0]){
         var reader = new FileReader();
-
-        reader.onload = function (e) {
-            $('#detecting-preview')
-                .attr('src', e.target.result);
+        reader.onload = function(e){
+            $('.detecting-preview-img').attr('src', e.target.result);
         };
-
         reader.readAsDataURL(input.files[0]);
     }
 }
+
 $(document).ready(function(){
 
     $('.recent-item').click(function(){
