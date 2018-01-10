2a1d4e3370b7ec72d2f1b94f51959b6852a4c514
Ahreum Han <reum712@gmail.com>
2018-01-10 13:09:51 +0900

    Tagging result 문제 2개 남음.

--- dist/stylens.js
+++ dist/stylens.js
@@ -17,67 +17,66 @@ $(document).ready(function(){
         $('.detecting-preview-img').attr('src', path);
     });
 
-});
-
-/*
-    [작동안함]
-    이미지 로드가 끝난 후에, 이미지의 가로 세로 크기를 구해서
-    movable layer의 parents layer크기를 교친다.
-*/
-$('.detecting-preview-img').one('load', function() {
-    var detectingWrapW = $(this).outerWidth();
-    var detectingWrapH = $(this).outerHeight();
-    $('.detecting-wrap').css({
-        'width': detectingWrapW,
-        'height': detectingWrapH
+    /*
+        이미지 로드가 끝난 후에, 이미지의 가로 세로 크기를 구해서
+        movable layer의 parents layer크기를 고친다.
+    */
+    $('.detecting-preview-img').one('load', function() {
+        var detectingWrapW = $(this).outerWidth();
+        var detectingWrapH = $(this).outerHeight();
+        $('.detecting-wrap').css({
+            'width': detectingWrapW,
+            'height': detectingWrapH
+        });
+    }).each(function() {
+        if(this.complete) $(this).load();
     });
-}).each(function() {
-    if(this.complete) $(this).load();
+
+    /* for Tagging Resut area */
+    interact('.detecting-square')
+      .draggable({
+        onmove: window.dragMoveListener,
+        restrict: {
+          restriction: 'parent',
+          elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
+        },
+      })
+      .resizable({
+        // resize from all edges and corners
+        edges: { left: true, right: true, bottom: true, top: true },
+
+        // keep the edges inside the parent
+        restrictEdges: {
+          outer: 'parent',
+          endOnly: true,
+        },
+
+        // minimum size
+        restrictSize: {
+          min: { width: 100, height: 50 },
+        },
+
+        inertia: true,
+      })
+      .on('resizemove', function (event) {
+        var target = event.target,
+            x = (parseFloat(target.getAttribute('data-x')) || 0),
+            y = (parseFloat(target.getAttribute('data-y')) || 0);
+
+        // update the element's style
+        target.style.width  = event.rect.width + 'px';
+        target.style.height = event.rect.height + 'px';
+
+        // translate when resizing from top or left edges
+        x += event.deltaRect.left;
+        y += event.deltaRect.top;
+
+        target.style.webkitTransform = target.style.transform =
+            'translate(' + x + 'px,' + y + 'px)';
+
+        target.setAttribute('data-x', x);
+        target.setAttribute('data-y', y);
+        target.textContent = Math.round(event.rect.width) + '\u00D7' + Math.round(event.rect.height);
+      });
+
 });
-
-/* for Tagging Resut area */
-interact('.detecting-square')
-  .draggable({
-    onmove: window.dragMoveListener,
-    restrict: {
-      restriction: 'parent',
-      elementRect: { top: 0, left: 0, bottom: 1, right: 1 }
-    },
-  })
-  .resizable({
-    // resize from all edges and corners
-    edges: { left: true, right: true, bottom: true, top: true },
-
-    // keep the edges inside the parent
-    restrictEdges: {
-      outer: 'parent',
-      endOnly: true,
-    },
-
-    // minimum size
-    restrictSize: {
-      min: { width: 100, height: 50 },
-    },
-
-    inertia: true,
-  })
-  .on('resizemove', function (event) {
-    var target = event.target,
-        x = (parseFloat(target.getAttribute('data-x')) || 0),
-        y = (parseFloat(target.getAttribute('data-y')) || 0);
-
-    // update the element's style
-    target.style.width  = event.rect.width + 'px';
-    target.style.height = event.rect.height + 'px';
-
-    // translate when resizing from top or left edges
-    x += event.deltaRect.left;
-    y += event.deltaRect.top;
-
-    target.style.webkitTransform = target.style.transform =
-        'translate(' + x + 'px,' + y + 'px)';
-
-    target.setAttribute('data-x', x);
-    target.setAttribute('data-y', y);
-    target.textContent = Math.round(event.rect.width) + '\u00D7' + Math.round(event.rect.height);
-  });
