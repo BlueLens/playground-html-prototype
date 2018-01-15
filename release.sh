dfb816afee28016df329379d9ecdf7a6447b2f88
Daesub Kim <rano@bluehack.net>
2018-01-15 23:06:50 +0900

    modified release.sh file.

--- release.sh
+++ release.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
+browserify main.js > bundle.js
+
 TOP="/Users/daesubkim/Desktop/STYL/web/playground-html-prototype"
-s3cmd sync --exclude=".git/*" --exclude=".idea/*" --exclude="README.md" --exclude="release.sh" --exclude="LICENSE" --recursive --default-mime-type="text/html" --guess-mime-type $TOP/* s3://playground.stylelens.io -c ~/.s3cfg
+s3cmd sync --exclude=".git/*" --exclude=".idea/*" --exclude="README.md" --exclude="release.sh" --exclude="LICENSE" --exclude="node_modules/*" --exclude="main.js" --exclude="*.json" --recursive --default-mime-type="text/html" --guess-mime-type $TOP/* s3://playground.stylelens.io -c ~/.s3cfg
