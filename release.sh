3a0ffab924b397e42fcf19be6ac3af6507d41646
Daesub Kim <rano@bluehack.net>
2018-01-22 20:28:35 +0900

    s3ignore added.

--- release.sh
+++ release.sh
@@ -3,4 +3,4 @@
 browserify main.js > bundle.js
 
 TOP="/Users/daesubkim/Desktop/STYL/web/playground-html-prototype"
-s3cmd sync --exclude=".git/*" --exclude=".idea/*" --exclude="README.md" --exclude="release.sh" --exclude="LICENSE" --exclude="node_modules/*" --exclude="main.js" --exclude="*.json" --recursive --default-mime-type="text/html" --guess-mime-type $TOP/* s3://playground.stylelens.io -c ~/.s3cfg
+s3cmd sync --exclude-from .s3ignore $TOP/ --default-mime-type="text/html" --guess-mime-type $TOP/* s3://playground.stylelens.io -c ~/.s3cfg
