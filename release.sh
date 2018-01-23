bdd520efc84809f985899ab4bb03eb1a94aa6c26
Daesub Kim <rano@bluehack.net>
2018-01-23 22:14:09 +0900

    Resolved [STYL-133] : modified s3 endpoint

--- release.sh
+++ release.sh
@@ -3,4 +3,4 @@
 browserify main.js > bundle.js
 
 TOP="/Users/daesubkim/Desktop/STYL/web/playground-html-prototype"
-s3cmd sync --exclude-from .s3ignore $TOP/ --default-mime-type="text/html" --guess-mime-type $TOP/* s3://playground.stylelens.io -c ~/.s3cfg
+s3cmd sync --exclude-from .s3ignore $TOP/ --default-mime-type="text/html" --guess-mime-type $TOP/* s3://playground.stylens.io -c ~/.s3cfg
